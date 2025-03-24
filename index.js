cec5719bb2479616573fb55ed9b1fa4423f37a1b
unknown <rjp237@gmail.com>
2025-03-24 15:20:09 +0530

    Ready to host

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 require('dotenv').config()
-console.log(process.env.SECRET);
- 
- const express = require('express');
+// console.log(process.env.SECRET);
+
+const express = require('express');
 const mongoose = require("mongoose");
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const ExpressError = require('./utils/express_error.js');
-const {joiSchema,reviewSchema}=require("./joiSchema.js");
+const { joiSchema, reviewSchema } = require("./joiSchema.js");
 
 const wrapAsync = require("./utils/wrapAsync.js");
 
 
-const lisingsRouter=require('./routes/listing.js');
-const reviewRouter=require('./routes/reviews.js');
-const userRouter=require('./routes/user.js');
+const lisingsRouter = require('./routes/listing.js');
+const reviewRouter = require('./routes/reviews.js');
+const userRouter = require('./routes/user.js');
 
 ///For session
 const passport = require('passport');
-const expressSession=require('express-session');
-const passportLocalStrategy=require('passport-local');
-const User=require('./models/user.js');
+const expressSession = require('express-session');
+const MongoStore = require('connect-mongo');
+const passportLocalStrategy = require('passport-local');
+const User = require('./models/user.js');
 
 
 const app = express();
 const port = 8080;
-
+const dbUrl = process.env.ATLASDB_URL;
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, '/public')));
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SESSION_SECRET
+    },
+    touchAfter: 24 * 3600
+})
 
-app.use(expressSession({secret:'MySecretCode',resave:false,saveUninitialized:true}));
+
+app.use(expressSession({ store, secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -56,12 +65,13 @@ main().then(() => {
 });
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/airbnbDB');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/airbnbDB');
+    await mongoose.connect(dbUrl);
 }
 
-app.use((req,res,next)=>{
-   res.locals.currUser= req.user;
-   next();
+app.use((req, res, next) => {
+    res.locals.currUser = req.user;
+    next();
 });
 
 
@@ -75,9 +85,9 @@ app.get("/", (req, res) => {
 
 
 
-app.use('/listings',lisingsRouter);
-app.use('/listings/:id/reviews',reviewRouter);
-app.use('/',userRouter);
+app.use('/listings', lisingsRouter);
+app.use('/listings/:id/reviews', reviewRouter);
+app.use('/', userRouter);
 
 
 
@@ -89,6 +99,6 @@ app.all("*", (req, res, next) => {
 });
 app.use((err, req, res, next) => {
 
-    let { statusCode=500, message="Something Went Wrong" } = err;
+    let { statusCode = 500, message = "Something Went Wrong" } = err;
     res.status(statusCode).send(message);
 });
